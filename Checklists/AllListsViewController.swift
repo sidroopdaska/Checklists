1dd43644580cd28c36ba1f15177b7fce9eb7843d
Siddharth <sid3637@gmail.com>
2015-10-15 12:19:48 +0100

    Update AllListsViewController.swift
    corrected ViewDidLoad

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -15,12 +15,6 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = false
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem()
   }
   
   override func viewWillAppear(animated: Bool) {
